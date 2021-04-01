0110ba54eda5bcc053e25b3fde0010b537576525
Pip85 <78318926+Pip85@users.noreply.github.com>
2021-03-31 22:53:40 -0400

    Add a message

--- PyBank/mainbank.py
+++ PyBank/mainbank.py
@@ -1,47 +1,50 @@
-# Run modules to import for op system and csv files
+# Run modules to import for all op systems and csv files
 import os
 import csv
 
-# Store csv file path in variable
+# Store csv file path in a variable
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
+# Create lists to hold months and profit_loss information
 months = []
 profit_loss = []
-# month_pl_d = {}
 
-# Open/read file
+# Open/read file & store header
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
+
     csv_header = next(csvreader)
     print(csv_header)
+
     lines = csvfile.readlines()
     
+    # create variable to hold csv data without the header
     body = lines[0:]
-         
-#         # for line in csv.DictReader(csvreader):
-#     #     print(line)
-
+   
+   # Move months and profit_loss data into lists
     for line in body:
         data = line.split(',')
         months.append(data[0])
         profit_loss.append(int(data[1]))
 
-print(months)
-print(profit_loss)
 
 num_months = len(months)
 net_profit = sum(profit_loss)
 
-print(num_months)
-print(net_profit)
+print(months)
+print(profit_loss)
 
+# print(num_months)
+# print(net_profit)
+
+# Create new lists to hold months and profit/loss data without first row to calculate change in profit/loss
 profit_loss2 = profit_loss[1:]
 months2 = months[1:]
 
 print(profit_loss2)
 print(months2)
 
-
+# zip months and profit/loss lists together to compare profit/loss
 change=[]
 zip_list = zip(profit_loss2, profit_loss)
 for profit_loss2_i, profit_loss_i in zip_list:
@@ -49,26 +52,33 @@ for profit_loss2_i, profit_loss_i in zip_list:
 
 print(change)
 
+# Calculate average change in profit/loss
 avg_change = round(sum(change)/len(change),2)
 
 print(avg_change)
 
-# keys = ['a', 'b', 'c']
-# values = [1, 2, 3]
-# dictionary = dict(zip(keys, values))
-# print(dictionary) # {'a': 1, 'b': 2, 'c': 3}
-
+# Pull data without first row into a dictionary
 dictionary = dict(zip(months2, change))
 
 print(dictionary)
 
+# Sort the profit/loss values in ascending order
 import operator
 plsort = sorted(dictionary.items(), key=operator.itemgetter(1))
 
 print(plsort)
 
-greatinc = plsort[-1]
+# Calculate the greatest increase/decreases in profit/loss by month
+greatinc = (plsort[-1])
 print(greatinc)
 
 greatdec = plsort[0]
 print(greatdec)
+
+
+# Create analysis of profit/loss
+print("Financial Analysis")
+print("-----------------------------------")
+print("Total Months:  $" + str(num_months))
+print("Average Change:  $" + str(avg_change))
+print("Greatest Increase in Profits:  " + str((greatinc).split(",")))
